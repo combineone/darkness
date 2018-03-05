6d6e138b9229498127cb68728293a9a85c03e11e
combineone <im@combine.one>
2018-03-05 23:57:13 +0300

    Refactoring for new glutin

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
-extern crate libc;
 extern crate gl;
 extern crate glutin;
 
+use glutin::GlContext;
+
 fn main() {
-    let events_loop = glutin::EventsLoop::new();
+    let mut events_loop = glutin::EventsLoop::new();
     let window = glutin::WindowBuilder::new()
         .with_title(r#"Darking Engine"#)
-        .with_dimensions(1024, 768)
-        .with_vsync()
-        .build(&events_loop)
-        .unwrap();
-    let mut running = true;
+        .with_dimensions(1024, 768);
+    let context = glutin::ContextBuilder::new()
+        .with_vsync(true);
+    let gl_window = glutin::GlWindow::new(window, context, &events_loop).unwrap();
 
-    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
     unsafe {
-        window.make_current().unwrap();
-        gl::ClearColor(0.6, 0.6, 0.6, 1.0);
+        gl_window.make_current().unwrap();
+        gl::load_with(|symbol| gl_window.get_proc_address(symbol) as *const _);
+        gl::ClearColor(0.4, 0.4, 0.4, 1.0);
     }
 
+    let mut running = true;
     while running {
         events_loop.poll_events(|event| {
             match event {
-                glutin::Event::WindowEvent{ event: glutin::WindowEvent::Closed, .. } => {
-                    running = false;
+                glutin::Event::WindowEvent{ event, .. } => match event {
+                    glutin::WindowEvent::Closed => running = false,
+                    glutin::WindowEvent::Resized(w, h) => gl_window.resize(w, h),
+                    _ => ()
                 },
                 _ => ()
             }
@@ -32,6 +35,6 @@ fn main() {
             gl::Clear(gl::COLOR_BUFFER_BIT);
         }
 
-        window.swap_buffers().unwrap();
+        gl_window.swap_buffers().unwrap();
     }
 }
